88a4ae81f73e10abec97fd53176214bc03362db5
Oscar Lastra <56526315+lastra-dev@users.noreply.github.com>
2021-06-14 22:20:53 -0500

    Add camera permision

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as sys_paths;
 
 class ImageInput extends StatefulWidget {
   @override
@@ -8,6 +11,20 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File? _storedImage;
+
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+    final imageFile = await picker.getImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+    setState(() {
+      _storedImage = File(imageFile!.path);
+    });
+    final appDir = await sys_paths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile!.path);
+    final savedImage = await _storedImage!.copy('${appDir.path}/$fileName');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +55,7 @@ class _ImageInputState extends State<ImageInput> {
             icon: const Icon(
               Icons.camera,
             ),
-            onPressed: () {},
+            onPressed: _takePicture,
             label: const Text('Take Picture'),
           ),
         ),
